e0a5089ee3f04aa28c1b519a64e6ab94d29436cb
Syama Ravi Teja Jerrypothula <48052253+syamrt@users.noreply.github.com>
2023-08-23 11:30:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas as pd
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt');
+my_fruit_list = my_fruit_list.set_index('Fruit');
 streamlit.title('Indian Healthy Diner');
 
 streamlit.header('Breakfast Menu');
