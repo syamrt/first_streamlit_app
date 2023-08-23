43ff06133d3c94619844f93a508b5d8650a97ca6
Syama Ravi Teja Jerrypothula <48052253+syamrt@users.noreply.github.com>
2023-08-23 11:26:54 -0500

    Update streamlit_app.py
    tables shifted down after heading

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 import pandas as pd
 
-my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(my_fruit_list);
 streamlit.title('Indian Healthy Diner');
 
 streamlit.header('Breakfast Menu');
@@ -11,3 +9,5 @@ streamlit.text('Kale, Spinach & Rocket Smoothie');
 streamlit.text('Hard-Boiled Free-Range Egg');
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list);
