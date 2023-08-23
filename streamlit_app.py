218ac174e79080dd0b944edac5c211a6f9ed1f64
Syama Ravi Teja Jerrypothula <48052253+syamrt@users.noreply.github.com>
2023-08-23 10:55:12 -0500

    Update streamlit_app.py
    Added one more header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('Indian Healthy Diner');
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach & Rocket Smoothie');
+streamlit.text('Hard-Boiled Free-Range Egg');
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
